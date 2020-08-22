19d88295ff9ffd204f0f23580b7eb49c64228f42
MAKSS <kazemi914@live.com>
2020-08-22 21:25:02 +0430

    Package got updated!

--- index.cjs
+++ index.cjs
@@ -1,13 +1,13 @@
-'use strict'
+'use strict';
 function ConvertNumbers(inputValue) {
-  if (inputValue) return undefined
+  if (inputValue) return undefined;
   return inputValue
     .replace(/[\u0660-\u0669]/g, function (number) {
       return number.charCodeAt(0) - 1632
     })
     .replace(/[\u06F0-\u06F9]/g, function (number) {
       return number.charCodeAt(0) - 1776
-    })
+    });
 }
 
-module.exports = ConvertNumbers
+module.exports = ConvertNumbers;
