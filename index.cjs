7a0c80b32229332a3b38c520bafaf7bcf34219d8
MAKSS <kazemi914@live.com>
2020-09-28 01:54:22 +0330

    Revert "Revert "Minor improvement in coding style""
    This reverts commit 1a4c04058935287640c76efa83ab4967561eccd2.

--- index.cjs
+++ index.cjs
@@ -3,10 +3,10 @@ function ConvertNumbers(inputValue) {
   if (!inputValue) return undefined;
   return inputValue
     .replace(/[\u0660-\u0669]/g, function (number) {
-      return number.charCodeAt(0) - 1632
+      return number.charCodeAt(0) - 1632;
     })
     .replace(/[\u06F0-\u06F9]/g, function (number) {
-      return number.charCodeAt(0) - 1776
+      return number.charCodeAt(0) - 1776;
     });
 }
 
