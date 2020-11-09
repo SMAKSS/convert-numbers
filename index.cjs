a709f75e22fd129cb626d77acd6c0fee229ed65b
MAKSS <kazemi914@live.com>
2020-11-09 15:52:36 +0330

    variable definition modified
    variable names got changed for a better understanding of their usage

--- index.cjs
+++ index.cjs
@@ -2,11 +2,11 @@
 function ConvertNumbers(inputValue) {
   if (!inputValue) return undefined;
   return inputValue
-    .replace(/[\u0660-\u0669]/g, function (number) {
-      return number.charCodeAt(0) - 1632;
+    .replace(/[\u0660-\u0669]/g, function (subString) {
+      return subString.charCodeAt(0) - 1632;
     })
-    .replace(/[\u06F0-\u06F9]/g, function (number) {
-      return number.charCodeAt(0) - 1776;
+    .replace(/[\u06F0-\u06F9]/g, function (subString) {
+      return subString.charCodeAt(0) - 1776;
     });
 }
 
